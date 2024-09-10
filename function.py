ab4842304caca95459c44436e176d5c65cecb736
Saugat954 <156522705+Saugat954@users.noreply.github.com>
2024-09-10 18:37:33 +1000

    Add files via upload

---
+++ function.py
@@ -0,0 +1,22 @@
+# Functions in python
+# Function helps to reduce code and makes user to understand code easily
+
+def addition(a,b):
+    sum= a+b
+    print(sum)
+
+def isGreater(a,b):
+    if(a>b):
+        print( a ,"is greather than ", b )
+    else:
+        print( b ,"is greather than ", a)
+
+
+a=20
+b=21
+c=22
+d=33
+addition(a,b)
+isGreater(a,b)
+addition(c,d)
+isGreater(c,d)
